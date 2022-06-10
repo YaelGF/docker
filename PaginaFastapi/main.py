ef7b5fb40ab825eca16c31a5fe70ef2fbfb3c727
YaelGF <1719110736@utectulancingo.edu.mx>
2022-06-10 20:57:00 +0000

    Ready

--- PaginaFastapi/main.py
+++ PaginaFastapi/main.py
@@ -1,10 +1,38 @@
 from typing import Union
-
 from fastapi import FastAPI
+import sqlite3
+from typing import List
+from pydantic import BaseModel
+
+class Respuesta(BaseModel):
+    message: str
+
+class Cliente(BaseModel):
+    id_cliente: int
+    nombre: str
+    email: str
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
+@app.get("/", response_model=Respuesta)
+async def index():
     return {"message": "Fast API"}
+
+@app.get("/clientes/", response_model=List[Cliente])
+async def clientes():
+    with sqlite3.connect("sql/clientes.sqlite") as conn:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("Select * From clientes")
+        response = cursor.fetchall()
+        return response
+
+@app.get("/clientes/{id_cliente}", response_model=Cliente)
+async def clientes(id_cliente: int):
+    with sqlite3.connect("sql/clientes.sqlite") as conn:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("Select * From clientes where id_cliente = {}".format(id_cliente))
+        response = cursor.fetchone()
+        return response
